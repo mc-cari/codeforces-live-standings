74c82af7fc574d8562fbdeecdfb4b71b77243997
mc-cari <mauriciocari2@gmail.com>
2023-04-08 23:55:27 -0400

    fix(ProblemStatus): add h-full to div to prevent the problem status from not grow inside its component

--- components/ProblemStatusEdu.tsx
+++ components/ProblemStatusEdu.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function ProblemStatusEdu({ problem } : { problem : ProblemResult }) {
   return (
-    <div>
+    <div className="h-full">
       {(problem.points > 0 || problem.rejectedAttemptCount > 0)
         && (
         <div className={`flex flex-col items-center text-xl justify-center py-1 h-full bg-${problem.points > 0 ? 'green-500' : 'red-500'}`}>

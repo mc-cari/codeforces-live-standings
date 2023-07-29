ded6843f6f1f94ca835872d08f6ade39b90bc0c3
mc-cari <mauriciocari2@gmail.com>
2023-07-29 03:46:31 -0400

    feat(index): add import users from contest option

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,73 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import getName from '../utils/getName';
 
 export default function Home() {
   const [handleText, setHandleText] = useState<string>('');
   const [contestId, setContestId] = useState<number>();
-  const [users, setUsers] = useState<string[]>([]);
+  const [usersHandles, setUsersHandles] = useState<string[]>([]);
   const [contestType, setContestType] = useState<string>('');
 
   const Router = useRouter();
 
   const handleStart = () => {
-    if (contestId && users && users.length > 0 && contestType) {
+    if (contestId && usersHandles && usersHandles.length > 0 && contestType) {
       Router.push({
         pathname: `/contests/${contestId}/standings`,
         query: {
           contestType,
-          handles: users,
+          handles: usersHandles,
         },
       });
     }
   };
 
-  const addHandles = () => {
-    let handles = handleText.split(',');
-    handles = handles.filter((handle) => !users.includes(handle));
+  const addHandles = (newHandles : string[]) => {
+    newHandles = newHandles.map((handle) => handle.trim());
+    newHandles = newHandles.filter((handle) => !usersHandles.includes(handle));
 
-    if (handles.length > 0) {
-      handles = handles.map((handle) => handle.trim());
-      setUsers((oldUsers) => [...oldUsers, ...handles]);
+    if (newHandles.length > 0) {
+      setUsersHandles((oldUsers) => [...oldUsers, ...newHandles]);
     }
+  };
+
+  const addInputHandles = () => {
+    const handles = handleText.split(',');
+    addHandles(handles);
+
     setHandleText('');
+  };
+
+  const importHandles = async () => {
+    try {
+      if (!contestId) {
+        throw new Error('Contest Id not set');
+      }
+
+      const standingsResponse = await fetch(
+        `${process.env.CF_API}contest.standings?contestId=${contestId}&count=65&showUnofficial=true`,
+        { mode: 'cors' },
+      );
+
+      if (!standingsResponse.ok) {
+        throw new Error('Failed to fetch standings data');
+      }
+
+      const standingsPromise = await standingsResponse.json();
+      const standings : Standings = standingsPromise.result;
+
+      const contestHandles : string[] = standings.rows.map((row) => getName(row.party));
+
+      addHandles(contestHandles);
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
   };
 
   return (
     <div className="flex flex-row bg-black h-screen items-center justify-center text-white">
-      <div className="flex w-1/2 h-1/2 items-center justify-center">
+      <div className="flex w-1/2 h-2/3 items-center justify-center">
         <div className="flex w-2/3 flex-col bg-slate-600 h-full p-3 justify-center border-8 border-zinc-800 rounded">
 
           <label className="mt-2" htmlFor="handles">
@@ -53,7 +86,7 @@ export default function Home() {
             <div className="w-1/2 flex justify-center">
               <button
                 className="w-2/3 bg-gray-400 hover:bg-gray-500 py-1 px-2 rounded"
-                onClick={addHandles}
+                onClick={addInputHandles}
                 type="button"
               >
                 Add
@@ -62,12 +95,12 @@ export default function Home() {
           </div>
 
           <div className="flex flex-col my-1 overflow-y-auto w-1/2 h-2/3 p-1 bg-zinc-800 rounded">
-            {users.map((user) => (
+            {usersHandles.map((user) => (
               <div key={user} className="flow-root justify-center py-1">
                 <p className="float-left">{user}</p>
                 <button
                   className="float-right  bg-gray-400 hover:bg-gray-500 py-1 px-1 rounded"
-                  onClick={() => setUsers(users.filter((userOld) => userOld !== user))}
+                  onClick={() => setUsersHandles(usersHandles.filter((userOld) => userOld !== user))}
                   type="button"
                 >
                   Remove
@@ -79,7 +112,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="flex w-1/2 h-1/2 items-center justify-center">
+      <div className="flex w-1/2 h-2/3 items-center justify-center">
         <div className="flex flex-col w-2/3 bg-slate-600 h-full p-3 justify-center border-8 border-zinc-800 rounded">
 
           <div className="flex flex-col h-1/3 justify-center">
@@ -99,6 +132,17 @@ export default function Home() {
                 name="contest"
                 onChange={(e) => setContestId(parseInt(e.target.value, 10))}
               />
+              <div className="w-1/2 flex justify-center">
+                {contestId && (
+                <button
+                  className="w-2/3 bg-gray-400 hover:bg-gray-500 py-1 px-2 rounded"
+                  onClick={importHandles}
+                  type="button"
+                >
+                  Import Handles
+                </button>
+                )}
+              </div>
             </div>
           </div>
 
